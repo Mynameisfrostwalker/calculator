c53de343569053595a84cff95ed9f0734d53f0fe
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-08 11:51:08 +0100

    created README and added extra buttons

--- scriptsfolder/script.js
+++ scriptsfolder/script.js
@@ -15,5 +15,23 @@ function divide(num1, num2) {
 }
 
 function operate(operator, numOne, numTwo) {
-    return operator(numOne, numTwo)
+    return (operator === 'add') ? add(numOne, numTwo) :
+        (operator === 'subtract') ? subtract(numOne, numTwo) :
+        (operator === 'multiply') ? multiply(numOne, numTwo) :
+        (operator === 'divide') ? divide(numOne, numTwo) : undefined
 }
+
+function calculate(event) {
+    console.log(event)
+}
+
+const buttons = document.querySelectorAll('div.buttons')
+buttons.forEach((button) => { button.addEventListener('click', calculate) })
+    /* create a nodelist of every button, 
+    iterate through each button and add an event listener, 
+    event listener calls a function fde on click. 
+    fde stores event.id in an array when the array 
+    length is 3 its contents are passed into the operate
+    function the out put is added as the first item of the array
+    and the process is repeated until the equal to button is pressed
+    */
