617d5c287d1e0f3a9ffe94d085ab026eeb2dbdc4
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-08 17:12:36 +0100

    Calculator logic completed

--- scriptsfolder/script.js
+++ scriptsfolder/script.js
@@ -1,3 +1,10 @@
+const store = document.querySelector("div#store");
+const mainDisplay = document.querySelector("div#mainDisplay");
+const paraStore = document.createElement("p");
+const paraMainDisplay = document.createElement("p");
+store.appendChild(paraStore);
+mainDisplay.appendChild(paraMainDisplay);
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -15,14 +22,112 @@ function divide(num1, num2) {
 }
 
 function operate(operator, numOne, numTwo) {
-    return (operator === 'add') ? add(numOne, numTwo) :
-        (operator === 'subtract') ? subtract(numOne, numTwo) :
-        (operator === 'multiply') ? multiply(numOne, numTwo) :
-        (operator === 'divide') ? divide(numOne, numTwo) : undefined
+    return (operator === '+') ? add(numOne, numTwo) :
+        (operator === '-') ? subtract(numOne, numTwo) :
+        (operator === '*') ? multiply(numOne, numTwo) :
+        (operator === '/') ? divide(numOne, numTwo) : undefined
+}
+
+let input = [];
+let problem = [];
+
+function number(num) {
+    if (input.includes(".")) {
+        if (num === ".") {
+            return;
+        }
+    }
+    input.push(num);
+}
+
+function operation(operator) {
+    let joined = input.join('');
+    if (joined !== '') {
+        problem.push(joined);
+    }
+    input = [];
+    if (problem.length !== 0 || /[+-]/.test(operator)) {
+        input.push(operator);
+    }
+    let operatorJoined = input.join("");
+    if (/[*+-\/](?![0-9])/g.test(problem[problem.length - 1]) && /[*\/]/.test(operator)) {
+        problem[problem.length - 1] = operatorJoined;
+        input = [];
+        return;
+    }
+    if (operatorJoined !== "" && /[\d]/.test(problem[problem.length - 1])) {
+        problem.push(operatorJoined);
+        input = [];
+    }
+}
+
+function backspace() {
+    if (input.length === 0) {
+        input = problem[problem.length - 1].split("")
+        problem.pop();
+        input.pop()
+    } else {
+        input.pop()
+    }
+}
+
+function clear() {
+    problem = [];
+    input = [];
+}
+
+function equals() {
+    let join = input.join("");
+    if (join !== "") {
+        problem.push(join);
+        input = []
+    }
+    if (problem.length <= 3) {
+        let finalResult = operate(problem[1], parseInt(problem[0]), parseInt(problem[2]));
+        problem = [];
+        input = finalResult.toString().split("");
+    }
+}
+
+function resolve() {
+    let finalResult = operate(problem[1], parseInt(problem[0]), parseInt(problem[2]));
+    problem.splice(0, 3, problem[0] = finalResult);
 }
 
 function calculate(event) {
-    console.log(event)
+    let regex = /[*+-\/]/g;
+    let numreg = /[0-9.]/;
+    if (numreg.test(event.target.id)) {
+        number(event.target.id)
+    }
+    if (regex.test(event.target.id)) {
+        operation(event.target.id)
+    }
+
+    if (event.target.id === "backspace") {
+        backspace()
+    }
+
+    if (event.target.id === "clear") {
+        clear()
+    }
+
+    if (event.target.id === "equals") {
+        equals()
+    }
+    let numOfNum = 0;
+    for (let i = 0; i < problem.length; i++) {
+        if (/[0-9]/.test(parseInt(problem[i]))) {
+            numOfNum += 1
+            if (numOfNum === 2) {
+                resolve()
+            }
+        }
+    }
+    let joinInput = input.join("");
+    let joinProblem = problem.join("");
+    paraMainDisplay.textContent = joinInput;
+    paraStore.textContent = joinProblem;
 }
 
 const buttons = document.querySelectorAll('div.buttons')
